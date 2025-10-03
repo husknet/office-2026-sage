3543dba8dd5265450fa89f77faafebab242200ad
hurt the kid <127146284+husknet@users.noreply.github.com>
2025-10-03 11:09:35 -0400

    Delete app/page.js

--- app/page.js
+++
@@ -1,7 +0,0 @@
-export const runtime = 'edge';
-
-export default function Home() {
-  // This page won't be visible since [...path] catches root
-  // But it's required by Next.js app router
-  return null;
-}
